01a841b4d8f1200b32fed39dab85279c463e9f0e
Aziza <azizamukashkyzy@gmail.com>
2024-08-28 14:49:30 +0600

    🎉 Event, User models created

--- src/project/models/user.py
+++ src/project/models/user.py
@@ -1,32 +1,19 @@
 from __future__ import annotations
 
-from sqlalchemy import (
-    Boolean,
-    DateTime,
-    ForeignKey,
-    Integer,
-    String,
-    select
- 
-)
+from sqlalchemy import Boolean, Integer, String, select
 
 
-from sqlalchemy.orm import (
-    Mapped,
-    MappedAsDataclass,
-    Session,
-    mapped_column
-)
+from sqlalchemy.orm import Mapped, MappedAsDataclass, Session, mapped_column
 
 from ..database import Base
 
 
 class User(MappedAsDataclass, Base, unsafe_hash=True):
     __tablename__ = "user"
-  
+
     id: Mapped[int] = mapped_column(Integer, init=False, primary_key=True)
     picture_url: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
-    username : Mapped[str] = mapped_column(String, nullable=False, unique=True)
+    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
     first_name: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
     last_name: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
     email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
@@ -35,10 +22,7 @@ class User(MappedAsDataclass, Base, unsafe_hash=True):
     @property
     def full_name(self) -> str:
         return f"{self.first_name} {self.last_name}"
-    
 
     @staticmethod
     def get_by_user_id(session: Session, user_id: int) -> User | None:
         return session.scalar(select(User).where(User.user_id == user_id))
-  
-
