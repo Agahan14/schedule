7b37be18ccd65bc0ae028b0e2edb7db189a463d3
Artur <artur@glbalify.xyz>
2024-08-28 18:19:56 +0600

    💩 🚧 new models availability and limit models

---
+++ src/project/utils/enums.py
@@ -0,0 +1,17 @@
+from enum import Enum
+
+
+class WeekDay(str, Enum):
+    MONDAY = "Monday"
+    TUESDAY = "Tuesday"
+    WEDNESDAY = "Wednesday"
+    THURSDAY = "Thursday"
+    FRIDAY = "Friday"
+    SATURDAY = "Saturday"
+    SUNDAY = "Sunday"
+
+class TimeType(str, Enum):
+    # WEEKS = "Weeks"
+    DAYS = "days"
+    HOURS = "Hours"
+    MINUTES = "Minutes"
