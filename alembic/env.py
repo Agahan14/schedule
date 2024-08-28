d19bdce845230283c26b68e5ee3a17b678c2ee59
nooruzbaiibraimov <nooruzbay@gmail.com>
2024-08-28 19:50:48 +0600

    Added alembic and fixed model fields. Because of this error "Python dataclasses error encountered when creating dataclass for 'Event': TypeError("non-default argument 'url' follows default argument")." Fixed some methods in models

--- alembic/env.py
+++ alembic/env.py
@@ -8,7 +8,6 @@ from alembic import context
 from src.project.database import Base, DATABASE_URL
 from src.project.models.user import User
 
-
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
--- src/project/models/user.py
+++ src/project/models/user.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Integer, select
+from sqlalchemy import String, Integer, select, Boolean
 from sqlalchemy.orm import MappedAsDataclass, Mapped, mapped_column, Session
 from sqlalchemy.testing.pickleable import User
 from collections.abc import Sequence
@@ -11,30 +11,27 @@ class User(MappedAsDataclass, Base, unsafe_hash=True):
     id: Mapped[int] = mapped_column(Integer, init=False, primary_key=True)
     email: str = mapped_column(String(255), unique=True, nullable=False)
     password: str = mapped_column(String(255), nullable=False)
+    username: Mapped[str] = mapped_column(String, nullable=True, unique=True)
+    picture_url: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
+    first_name: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
+    last_name: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
+    is_verified: Mapped[bool] = mapped_column(Boolean, nullable=True, default=False)
 
     def __repr__(self) -> str:
         return f"<User(id={self.id}, email={self.email})>"
 
     @staticmethod
+    def get_all(session_schedule: Session) -> Sequence[User]:
+        return (session_schedule.scalars(select(User))).all()
+
+    @staticmethod
     def get_by_id(session_schedule: Session, id: int) -> User | None:
         return session_schedule.scalar(select(User).where(User.id == int(id)))
-    picture_url: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
-    username: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    first_name: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
-    last_name: Mapped[str | None] = mapped_column(String, nullable=True, default=None)
-    email: Mapped[str] = mapped_column(String, nullable=False, unique=True)
-    is_verified: Mapped[bool] = mapped_column(Boolean, nullable=False, default=False)
-
-    @property
-    def full_name(self) -> str:
-        return f"{self.first_name} {self.last_name}"
 
     @staticmethod
     def get_by_email(session_schedule: Session, email: str) -> User | None:
         return session_schedule.scalar(select(User).where(User.email == email))
 
-    @staticmethod
-    def get_all(session_schedule: Session) -> Sequence[User]:
-        return (session_schedule.scalars(select(User))).all()
-    def get_by_user_id(session: Session, user_id: int) -> User | None:
-        return session.scalar(select(User).where(User.user_id == user_id))
+    @property
+    def full_name(self) -> str:
+        return f"{self.first_name} {self.last_name}"
