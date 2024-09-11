bedf37e3008501055836c1d39f09fdff067c8406
Aziza <azizamukashkyzy@gmail.com>
2024-09-11 18:24:04 +0600

    ✅ Events page for events empty list

--- src/project/routers/event.py
+++ src/project/routers/event.py
@@ -3,9 +3,11 @@ import os
 from fastapi import APIRouter, Depends, Query, Request
 from sqlalchemy.orm import Session
 from starlette.templating import Jinja2Templates
-
+from pydantic import BaseModel
 from ..dependencies import get_db_session
 from ..models import Event
+
+
 
 current_dir = os.path.dirname(os.path.realpath(__file__))
 template_dir = os.path.join(current_dir, "..", "templates")
@@ -15,10 +17,14 @@ templates = Jinja2Templates(directory=template_dir)
 router = APIRouter(prefix="/event")
 
 
-@router.get("/event", tags=["event"])
-async def academy(request: Request, session: Session = Depends(get_db_session)):
+@router.get("/", tags=["event"])
+async def events(request: Request, session: Session = Depends(get_db_session)):
     events = Event.get_all(session)
     return templates.TemplateResponse(
         "event.html",
         {"request": request, "events": events},
     )
+
+
+
+
