98d6cd4396e0f56a3812533d56712079a3f964e2
nooruzbaiibraimov <nooruzbay@gmail.com>
2024-09-11 17:11:29 +0600

    :sparkles: Introducing new features. user can see and update profile picture

--- src/project/routers/main.py
+++ src/project/routers/main.py
@@ -3,9 +3,12 @@ import os
 
 from fastapi import (
     APIRouter,
-    Request,
+    Request, Depends,
 )
 from fastapi.templating import Jinja2Templates
+from sqlalchemy.orm import Session
+
+from ..dependencies import get_current_user, get_db_session
 
 router = APIRouter()
 current_dir = os.path.dirname(os.path.realpath(__file__))
@@ -14,6 +17,7 @@ template_dir = os.path.join(current_dir, "..", "templates")
 templates = Jinja2Templates(directory=template_dir)
 
 
-@router.get("/")
-def main(request: Request):
-    return templates.TemplateResponse("index.html", {"request": request})
+@router.get("/")  # Adjust the route as needed
+async def main(request: Request, session: Session = Depends(get_db_session)):
+    current_user = get_current_user(request, session)
+    return templates.TemplateResponse("index.html", {"request": request, "user": current_user})
