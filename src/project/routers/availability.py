b28740d5361018ad01cec2e2347cd32f3867a826
nooruzbaiibraimov <nooruzbay@gmail.com>
2024-09-21 20:30:46 +0600

    :art: Improving structure / format of the code. Checked with ruff and refactored the path to "/availability/{id}" in availability.py

--- src/project/routers/availability.py
+++ src/project/routers/availability.py
@@ -1,5 +1,3 @@
-import os
-
 from fastapi import (
     APIRouter,
     Depends,
@@ -7,10 +5,9 @@ from fastapi import (
 )
 from sqlalchemy.orm import Session
 from starlette.responses import HTMLResponse
-from starlette.templating import Jinja2Templates
 
-from src.project.dependencies import get_current_user, get_db_session, current_dir, template_dir, templates
-from src.project.models import Availability
+from ..dependencies import current_dir, get_current_user, get_db_session, template_dir, templates
+from ..models import Availability
 
 current_dir = current_dir
 template_dir = template_dir
@@ -25,22 +22,19 @@ async def get_availability(request: Request, session: Session = Depends(get_db_s
     return templates.TemplateResponse("availability/list_availability.html", {"request": request, "user": current_user})
 
 
-@router.api_route(
-    "/create_availability", methods=["GET", "POST"], tags=["availability"], response_class=HTMLResponse
-)
+@router.api_route("/create_availability", methods=["GET", "POST"], tags=["availability"], response_class=HTMLResponse)
 async def post_availability():
     pass
 
 
-@router.api_route(
-    "/availability_detail/{id}", methods=["GET", "POST"], tags=["availability"], response_class=HTMLResponse
-)
+@router.api_route("/availability/{id}", methods=["GET", "POST"], tags=["availability"], response_class=HTMLResponse)
 async def availability_detail(id: int, request: Request, session: Session = Depends(get_db_session)) -> HTMLResponse:
     if request.method == "GET":
         current_user = get_current_user(request, session)
         availability = Availability.get_user_availability(session, aval_id=id, user_id=current_user.id)
         return templates.TemplateResponse(
-            "availability/availability_detail.html", {"request": request, "user": current_user, "availability": availability}
+            "availability/availability_detail.html",
+            {"request": request, "user": current_user, "availability": availability},
         )
     elif request.method == "POST":
         current_user = get_current_user(request, session)
@@ -49,10 +43,6 @@ async def availability_detail(id: int, request: Request, session: Session = Depe
         availability.work_schedule = work_schedule
         session.commit()
         return templates.TemplateResponse(
-            "availability/availability_detail.html", {"request": request, "user": current_user, "availability": availability}
+            "availability/availability_detail.html",
+            {"request": request, "user": current_user, "availability": availability},
         )
-
-
-
-
-
