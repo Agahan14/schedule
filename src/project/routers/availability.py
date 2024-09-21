3c6edae5cf49a5a18b071f14fe67af766c4959f7
nooruzbaiibraimov <nooruzbay@gmail.com>
2024-09-21 20:21:01 +0600

    :recycle: Refactoring code. Editted some styles in list-availability.html

--- src/project/routers/availability.py
+++ src/project/routers/availability.py
@@ -9,13 +9,12 @@ from sqlalchemy.orm import Session
 from starlette.responses import HTMLResponse
 from starlette.templating import Jinja2Templates
 
-from src.project.dependencies import get_current_user, get_db_session
+from src.project.dependencies import get_current_user, get_db_session, current_dir, template_dir, templates
 from src.project.models import Availability
 
-current_dir = os.path.dirname(os.path.realpath(__file__))
-template_dir = os.path.join(current_dir, "..", "templates")
-
-templates = Jinja2Templates(directory=template_dir)
+current_dir = current_dir
+template_dir = template_dir
+templates = templates
 
 router = APIRouter()
 
@@ -23,7 +22,14 @@ router = APIRouter()
 @router.get("/availability", tags=["availability"], response_class=HTMLResponse)
 async def get_availability(request: Request, session: Session = Depends(get_db_session)) -> HTMLResponse:
     current_user = get_current_user(request, session)
-    return templates.TemplateResponse("availability.html", {"request": request, "user": current_user})
+    return templates.TemplateResponse("availability/list_availability.html", {"request": request, "user": current_user})
+
+
+@router.api_route(
+    "/create_availability", methods=["GET", "POST"], tags=["availability"], response_class=HTMLResponse
+)
+async def post_availability():
+    pass
 
 
 @router.api_route(
@@ -34,7 +40,7 @@ async def availability_detail(id: int, request: Request, session: Session = Depe
         current_user = get_current_user(request, session)
         availability = Availability.get_user_availability(session, aval_id=id, user_id=current_user.id)
         return templates.TemplateResponse(
-            "availability_detail.html", {"request": request, "user": current_user, "availability": availability}
+            "availability/availability_detail.html", {"request": request, "user": current_user, "availability": availability}
         )
     elif request.method == "POST":
         current_user = get_current_user(request, session)
@@ -43,5 +49,10 @@ async def availability_detail(id: int, request: Request, session: Session = Depe
         availability.work_schedule = work_schedule
         session.commit()
         return templates.TemplateResponse(
-            "availability_detail.html", {"request": request, "user": current_user, "availability": availability}
+            "availability/availability_detail.html", {"request": request, "user": current_user, "availability": availability}
         )
+
+
+
+
+
