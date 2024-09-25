44daa8bf5347043c31e32b4b9868448a3fc2a1a6
nooruzbaiibraimov <nooruzbay@gmail.com>
2024-09-25 15:05:49 +0600

    :sparkles: Introducing new features. Added vue component for create availabilty

--- src/project/static/vue/base.js
+++ src/project/static/vue/base.js
@@ -3,3 +3,7 @@ const { defineComponent, createApp } = Vue;
 const CreateEvent = defineComponent({
     template: "#create-event-template",
 });
+
+const CreateAvailability = defineComponent({
+    template: "#create-availability-template",
+});
--- src/project/static/vue/main.js
+++ src/project/static/vue/main.js
@@ -1,10 +1,20 @@
 createApp({
     components: {
         CreateEvent,
+        CreateAvailability,
     },
     data() {
         return {
             createEventOpened: false,
+            createAvailabilityOpened: false
         };
     },
+    methods: {
+        createAvailability() {
+            this.createAvailabilityOpened = true;
+        },
+        closeAvailability() {
+            this.createAvailabilityOpened = false;
+        }
+    }
 }).mount("#app");
